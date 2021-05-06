57c090ee947f7e5483c42ce0d32f3c4720580565
wanglinxiang <wanglinxiang@shantaijk.cn>
2021-05-06 15:56:51 +0800

    modify start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,5 @@ if ps aux | grep -i uwsgi &> /dev/null;then
   pkill -9 uwsgi
   sleep 2
 fi
+cat /dev/null >./ui.log
 /usr/local/bin/uwsgi --socket "0.0.0.0:8000" --wsgi-file wsgi.py --callable app --master --daemonize ./ui.log
